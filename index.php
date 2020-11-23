3d5c91b281419e6975ca09fab3c10f4095112d22
Ela Nuzul <elanuzul@gmail.com>
2020-11-23 13:33:31 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nulla explicabo animi quidem eius non dolore harum eligendi, at distinctio laborum repudiandae aliquam, sequi ullam! Dignissimos repudiandae architecto unde doloribus?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 
 </html>
